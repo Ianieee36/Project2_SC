f156bbc5ba2f1f3e0ebaac282938627b76c11f60
Ianieee36 <colitos169@gmail.com>
2025-10-16 13:11:28 +1300

    CREATE: CourseSelectionListener
    I create this interface class that serves as my listener or that connects with my view and controller with my programs functions such as adding student, adding course, enrolling, dropping, listing courses, searching courses.

---
+++ src/UI/CourseSelectionListener.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package UI;
+
+/**
+ *
+ * @author christian
+ */
+public interface CourseSelectionListener {
+    void onAddStudent(String id, String name);
+    void onAddCourse(String code, String title, int credits, String level);
+    void onEnroll(String studentId, String courseCode);
+    void onDrop(String studentId, String courseCode);
+    void onListCourses(String studentId);
+    void onSearchCourses(String query);
+}
